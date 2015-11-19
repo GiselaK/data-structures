523504277aa2f7bdf7d180b302e1ec598ba1b980
giselakay <giselakay>
2015-11-19 14:27:52 -0800

    Progress on LinkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,12 +1,29 @@
-var LinkedList = function() {
-  var list = {};
-  list.head = null;
-  list.tail = null;
+var LinkedList = function() { // Linked List class
+  var list = {}; // instance of LinkedList Class
+  list.head = null; // instance property head equals null
+  list.tail = null; // instance property head equals null
 
   list.addToTail = function(value) {
+    // list.head = !list.tail ? null : value;
+    if (!list.tail){ // no values in the list
+      list.head = Node(value); 
+      list.tail = Node(value);
+    } else if (!list.head.next) { // 1 value in the list, stored as both head and tail
+      list.head.next = Node(value);
+      // this.tail.next = Node(value);
+      list.tail = Node(value);
+    } else {                      // more than 1 item in list
+      // list.tail.next = Node(value);
+      list.head = list.head.next
+      list.tail = Node(value);
+    }
+    return list.tail;
   };
 
   list.removeHead = function() {
+    var value = list.head.value;
+    list.head = list.head.next;  
+    return value;
   };
 
   list.contains = function(target) {
