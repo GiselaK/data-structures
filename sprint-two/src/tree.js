fec50065303f0f11ec182f9080e9afefa9cb658a
giselakay <giselakay>
2015-11-19 19:10:09 -0800

    Complete Tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,9 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -12,10 +12,25 @@ var treeMethods = {};
 
 treeMethods.addChild = function(value) {
   // your code here
-  newTree.children = null;  // fix me
+  var tree = Tree(value);
+  this.children.push(tree);
+  tree.children = [];  // fix me
+  return tree;
 };
 
 treeMethods.contains = function(target) {
+  var wasFound = false;
+  var checkChildren = function(node){
+    if(node.value === target){
+      wasFound = true;
+      return;
+    } 
+    for (var i = 0; i<node.children.length; i++){
+        checkChildren(node.children[i]);
+    }
+  };
+  checkChildren(this);
+  return wasFound;
 };
 
 
