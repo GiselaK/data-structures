03053371d5ae644580dcd4b705e8cc50e0fd2e7f
giselakay <giselakay>
2015-11-20 11:01:09 -0800

    Complete Hash Table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,39 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (!this._storage[index]){
+    this._storage[index] = [];
+  }
+
+  for (var i = 0; i < this._storage[index].length; i++){
+    if (this._storage[index][i][0] === k){
+      this._storage[index][i][1] = v;
+      return;
+    }
+  }
+  var tuple = [k, v];
+  this._storage[index].push(tuple);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var array = this._storage[index];
+
+  if (array){
+    for (var i=0; i<array.length; i++){
+      if (array[i][0] === k){
+        return array[i][1];
+      }
+    }
+  }
+  
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var value = this._storage[index];
+  delete this._storage[index];
+  return value;
 };
 
 
