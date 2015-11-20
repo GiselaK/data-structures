4ea144ec3fc70aa2c2d6246b294b16da40aee81f
giselakay <giselakay>
2015-11-19 20:07:42 -0800

    Pass All Set Tests

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,19 +1,30 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
-  set._storage = undefined;
+  // only add unique values
+  if(this._storage.indexOf(item)=== -1){
+    this._storage.push(item)
+  }
 };
 
 setPrototype.contains = function(item) {
+  if(this._storage.indexOf(item) > -1){
+    return true;
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  var index = this._storage.indexOf(item);
+  if(index > -1){
+    this._storage[index] = undefined;
+  }
 };
 
 /*
