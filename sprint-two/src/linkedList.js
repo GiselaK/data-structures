14c2685e21b5e6cf0de989cdd7f4eae1023de27e
giselakay <giselakay>
2015-11-19 16:02:46 -0800

    Complete LinkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,29 +4,45 @@ var LinkedList = function() { // Linked List class
   list.tail = null; // instance property head equals null
 
   list.addToTail = function(value) {
-    // list.head = !list.tail ? null : value;
-    if (!list.tail){ // no values in the list
-      list.head = Node(value); 
-      list.tail = Node(value);
-    } else if (!list.head.next) { // 1 value in the list, stored as both head and tail
-      list.head.next = Node(value);
-      // this.tail.next = Node(value);
-      list.tail = Node(value);
-    } else {                      // more than 1 item in list
-      // list.tail.next = Node(value);
-      list.head = list.head.next
-      list.tail = Node(value);
+    var node = Node(value);
+    if(!list.tail){
+        // 1. empty list 
+      list.head = node;
+      list.tail = node;      
+    } else{
+        // 2. populated list
+      this.tail.next = node;
+      this.tail = node;      
     }
-    return list.tail;
   };
 
   list.removeHead = function() {
+    if (!list.head){
+      return;
+    }
     var value = list.head.value;
-    list.head = list.head.next;  
+    if(!list.head.next && list.head){
+      list.head = null
+      list.tail = null;
+    } else {
+      list.head = list.head.next;  
+    }
     return value;
   };
 
   list.contains = function(target) {
+    var checkNode = function  (node) {
+      if(node.value === target){
+        return true;
+      }
+      else if(!node.next){
+        return false;
+      }
+      else {
+        return checkNode(node.next);
+      }
+    }
+    return checkNode(list.head);
   };
 
   return list;
