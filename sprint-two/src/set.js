340eaefa3bceb11f925e3e8ec58d8f0c4525396a
giselakay <giselakay>
2015-11-19 19:22:57 -0800

    Complete Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,19 +1,25 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
-  set._storage = undefined;
+  this._storage.push(item);
 };
 
 setPrototype.contains = function(item) {
+  if (this._storage.indexOf(item) > -1){
+    return true;
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  var index = this._storage.indexOf(item);
+  this._storage[index] = undefined;
 };
 
 /*
