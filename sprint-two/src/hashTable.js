cd7b5059844435005ee30ab1de2ff09c444ae953
giselakay <giselakay>
2015-11-20 20:48:51 -0800

    Progress on Hash Table Advanced cases

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,9 +3,11 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._count = 0;
 };
 
 HashTable.prototype.insert = function(k, v) {
+  this._count++;
   var index = getIndexBelowMaxForKey(k, this._limit);
   if (!this._storage[index]){
     this._storage[index] = [];
@@ -15,10 +17,20 @@ HashTable.prototype.insert = function(k, v) {
     if (this._storage[index][i][0] === k){
       this._storage[index][i][1] = v;
       return;
-    }
+    }           
   }
   var tuple = [k, v];
   this._storage[index].push(tuple);
+  if (this._count / this._limit > 0.75) {
+    // double array size limit
+    this._limit *= 2;
+    // create new storage object
+    var tempStorage = {};
+    // reassign all values into new storage
+
+    // assign this._storage to the new storage
+    this._storage = tempStorage;
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
@@ -36,6 +48,7 @@ HashTable.prototype.retrieve = function(k) {
 };
 
 HashTable.prototype.remove = function(k) {
+  this._count--;
   var index = getIndexBelowMaxForKey(k, this._limit);
   var value = this._storage[index];
   delete this._storage[index];
