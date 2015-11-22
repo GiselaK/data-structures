f91bf93ee3667ade6727e903e81929b5d5da8858
giselakay <giselakay>
2015-11-21 16:19:41 -0800

    Complete Advance Hash Table Reqs

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -4,7 +4,38 @@ var HashTable = function() {
   this._items = 0;
 };
 
-HashTable.prototype.insert = function(k, v) {
+HashTable.prototype.rehash = function () {
+  // create new storage object
+  var tempStorage = LimitedArray(this._limit);
+  var index, newIndexArray;
+  // loop over all hash indices
+  this._storage.each(function (indexArray) {
+    // loop over each index array
+    _.each(indexArray, function (tuple) {
+      // put each key through hash function to generate a new index
+      index = getIndexBelowMaxForKey(tuple[0], this._limit);
+
+      // get array at index of new storage object
+      newIndexArray = tempStorage.get(index);
+
+       // check if there's NOT an array at index
+      if (!newIndexArray) {
+           // create an empty array at index
+           newIndexArray = [];
+      }
+      // push tuple to array 
+      newIndexArray.push(tuple);
+      
+      // set updated array into index of new storage object
+      tempStorage.set(index, newIndexArray);
+    });
+  });
+
+  // update reference to new storage
+  this._storage = tempStorage;
+};
+
+HashTable.prototype.insert = function (k, v) {
   this._count++;
   var index = getIndexBelowMaxForKey(k, this._limit);
   var tuple = [k, v];
@@ -32,6 +63,14 @@ HashTable.prototype.insert = function(k, v) {
   if (!collision){
     currentIndexArray.push(tuple);
     this._items++;
+
+    // check if it needs rehashing
+    if (this._items / this._limit > 0.75) {
+      // double limit
+      this._limit *= 2;
+      // rehash
+      this.rehash();
+    }
   }
   // set index with new array
   this._storage.set(index, currentIndexArray);
@@ -64,19 +103,26 @@ HashTable.prototype.remove = function(k) {
   var currentIndexArray = this._storage.get(index);
   var value;
   // loop over arrayatindex
-  for (var i = 0; i < currentIndexArray.length; i++) {
-    // if k equals arrayatindex[i][0]
-    if (currentIndexArray[i][0] === k){
-      // splice tupil out of array
-      value = currentIndexArray.splice(i, 1);
+  if (currentIndexArray) {
+    for (var i = 0; i < currentIndexArray.length; i++) {
+      // if k equals arrayatindex[i][0]
+      if (currentIndexArray[i][0] === k){
+        // splice tupil out of array
+        value = currentIndexArray.splice(i, 1);
+      }
     }
   }
   // set (index, spliced array)
   this._storage.set(index, currentIndexArray);
   // counter --
   this._items--;
+  // check if needs to be rehashed
+  if (this._items / this._limit < 0.25 && this._items > 0) {
+    this._limit /= 2;
+    this.rehash();
+  }
   // return val
-  return value[1];
+  return value ? value[1] : null;
 };
 
 
